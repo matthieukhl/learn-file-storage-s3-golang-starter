370495a52108d05f7b9d76b180b7238dd080d825
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-02-27 14:12:50 +0100

    added getVideoAspectRatio() helper function that retrieves video's aspect ratio from metadata using ffprobe command. Handler to upload video now stores into 'landscape', 'portrait' or 'other' folders in S3, based on video aspect ratio

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,286 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	http.MaxBytesReader(w, r.Body, 1<<30)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println(err)
+			respondWithError(w, http.StatusNotFound, "video not found", err)
+			return
+		}
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "error fetching video metadata", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		log.Println("user not owner of video")
+		respondWithError(w, http.StatusUnauthorized, "unauthorized: user not owner of the video", err)
+		return
+	}
+
+	const maxMemory = 1 << 30
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "failed to read media type from header", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "only jpeg and png files are supported", err)
+		return
+	}
+
+	tmpFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "failed to create temporary file on disk for video upload", err)
+		return
+	}
+	defer os.Remove(tmpFile.Name())
+	defer tmpFile.Close()
+
+	written, err := io.Copy(tmpFile, file)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "failed to write video data to tmp file", err)
+		return
+	}
+
+	log.Printf("Successfully copied %d bytes to %s\n", written, tmpFile.Name())
+
+	_, err = tmpFile.Seek(0, io.SeekStart)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "seek error", err)
+		return
+	}
+
+	fileExtension, err := mime.ExtensionsByType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error parsing mime type", err)
+		return
+	}
+
+	b := make([]byte, 32)
+	rand.Read(b)
+	fileKey := base64.RawURLEncoding.EncodeToString(b)
+
+	aspectRatio, err := getVideoAspectratio(tmpFile.Name())
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "failed to get video aspect ratio", err)
+		return
+	}
+
+	log.Println()
+	log.Println(aspectRatio)
+	log.Println()
+
+	var folder string
+	switch aspectRatio {
+	case "16:9":
+		folder = "landscape"
+	case "9:16":
+		folder = "portrait"
+	default:
+		folder = "other"
+
+	}
+
+	fileName := fmt.Sprintf("%s/%s%s", folder, fileKey, fileExtension[0])
+
+	// Upload to bucket
+	s3uploadParams := s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &fileName,
+		ContentType: &mediaType,
+		Body:        tmpFile,
+	}
+
+	output, err := cfg.s3Client.PutObject(r.Context(), &s3uploadParams)
+	if err != nil {
+		log.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "error uploading to s3", err)
+		return
+	}
+
+	if output != nil {
+		log.Println(output)
+	}
+
+	newVideoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, *s3uploadParams.Key)
+
+	videoMetadata.VideoURL = &newVideoURL
+
+	cfg.db.UpdateVideo(videoMetadata)
+	respondWithJSON(w, http.StatusCreated, "video upload successful")
+}
+
+func getVideoAspectratio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	log.Println(cmd.String())
+
+	output := FFProbeOutput{}
+	err := json.Unmarshal(stdout.Bytes(), &output)
+	if err != nil {
+		return "", err
+	}
+
+	aspectRatio := output.Streams[0].DisplayAspectRatio
+
+	if aspectRatio != "16:9" && aspectRatio != "9:16" {
+		return "other", nil
+	}
+
+	return aspectRatio, nil
+}
+
+type FFProbeOutput struct {
+	Streams []struct {
+		Index              int    `json:"index"`
+		CodecName          string `json:"codec_name,omitempty"`
+		CodecLongName      string `json:"codec_long_name,omitempty"`
+		Profile            string `json:"profile,omitempty"`
+		CodecType          string `json:"codec_type"`
+		CodecTagString     string `json:"codec_tag_string"`
+		CodecTag           string `json:"codec_tag"`
+		Width              int    `json:"width,omitempty"`
+		Height             int    `json:"height,omitempty"`
+		CodedWidth         int    `json:"coded_width,omitempty"`
+		CodedHeight        int    `json:"coded_height,omitempty"`
+		HasBFrames         int    `json:"has_b_frames,omitempty"`
+		SampleAspectRatio  string `json:"sample_aspect_ratio,omitempty"`
+		DisplayAspectRatio string `json:"display_aspect_ratio,omitempty"`
+		PixFmt             string `json:"pix_fmt,omitempty"`
+		Level              int    `json:"level,omitempty"`
+		ColorRange         string `json:"color_range,omitempty"`
+		ColorSpace         string `json:"color_space,omitempty"`
+		ColorTransfer      string `json:"color_transfer,omitempty"`
+		ColorPrimaries     string `json:"color_primaries,omitempty"`
+		ChromaLocation     string `json:"chroma_location,omitempty"`
+		FieldOrder         string `json:"field_order,omitempty"`
+		Refs               int    `json:"refs,omitempty"`
+		IsAvc              string `json:"is_avc,omitempty"`
+		NalLengthSize      string `json:"nal_length_size,omitempty"`
+		ID                 string `json:"id"`
+		RFrameRate         string `json:"r_frame_rate"`
+		AvgFrameRate       string `json:"avg_frame_rate"`
+		TimeBase           string `json:"time_base"`
+		StartPts           int    `json:"start_pts"`
+		StartTime          string `json:"start_time"`
+		DurationTs         int    `json:"duration_ts"`
+		Duration           string `json:"duration"`
+		BitRate            string `json:"bit_rate,omitempty"`
+		BitsPerRawSample   string `json:"bits_per_raw_sample,omitempty"`
+		NbFrames           string `json:"nb_frames"`
+		ExtradataSize      int    `json:"extradata_size"`
+		Disposition        struct {
+			Default         int `json:"default"`
+			Dub             int `json:"dub"`
+			Original        int `json:"original"`
+			Comment         int `json:"comment"`
+			Lyrics          int `json:"lyrics"`
+			Karaoke         int `json:"karaoke"`
+			Forced          int `json:"forced"`
+			HearingImpaired int `json:"hearing_impaired"`
+			VisualImpaired  int `json:"visual_impaired"`
+			CleanEffects    int `json:"clean_effects"`
+			AttachedPic     int `json:"attached_pic"`
+			TimedThumbnails int `json:"timed_thumbnails"`
+			NonDiegetic     int `json:"non_diegetic"`
+			Captions        int `json:"captions"`
+			Descriptions    int `json:"descriptions"`
+			Metadata        int `json:"metadata"`
+			Dependent       int `json:"dependent"`
+			StillImage      int `json:"still_image"`
+			Multilayer      int `json:"multilayer"`
+		} `json:"disposition"`
+		Tags struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			VendorID    string `json:"vendor_id"`
+			Encoder     string `json:"encoder"`
+			Timecode    string `json:"timecode"`
+		} `json:"tags,omitempty"`
+		SampleFmt      string `json:"sample_fmt,omitempty"`
+		SampleRate     string `json:"sample_rate,omitempty"`
+		Channels       int    `json:"channels,omitempty"`
+		ChannelLayout  string `json:"channel_layout,omitempty"`
+		BitsPerSample  int    `json:"bits_per_sample,omitempty"`
+		InitialPadding int    `json:"initial_padding,omitempty"`
+		Tags0          struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			VendorID    string `json:"vendor_id"`
+		} `json:"tags,omitempty"`
+		Tags1 struct {
+			Language    string `json:"language"`
+			HandlerName string `json:"handler_name"`
+			Timecode    string `json:"timecode"`
+		} `json:"tags,omitempty"`
+	} `json:"streams"`
+}
